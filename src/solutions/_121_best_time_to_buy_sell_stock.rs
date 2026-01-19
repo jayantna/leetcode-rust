3b0106e355a6b4a522cdc7b00b0cc838784d44ef
Jayant <jonty.nagle@gmail.com>
2026-01-19 16:52:19 +0530

    correction 121

--- src/solutions/_121_best_time_to_buy_sell_stock.rs
+++ src/solutions/_121_best_time_to_buy_sell_stock.rs
@@ -3,18 +3,20 @@ impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut min_b = prices[0];
         let mut profit = 0;
-        for i in (0..prices.len() - 1) {
+        for i in (0..prices.len()) {
+            println!("{:?}", i);
             if prices[i] < min_b {
                 min_b = prices[i];
             } else if prices[i] - min_b > profit {
                 profit = prices[i] - min_b;
             }
+            println!("iter {:?} {:?}", min_b, profit);
         }
         profit
     }
 
     pub fn run() {
-        let prices = vec![7, 1, 5, 3, 6, 4];
+        let prices = vec![1,2];
         let ans = Solution::max_profit(prices);
         println!("{:?}", ans);
     }
